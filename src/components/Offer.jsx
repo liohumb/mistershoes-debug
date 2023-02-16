5f7d0ecaf0121afa84dba6ec4be2a1cd6d7bfef4
liohumb <helloliohumb@gmail.com>
2023-02-16 15:36:58 +0100

    ticket 6: mise en place de la fonctionnalité copier le code promo dans le presse papier avec message de confirmation

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { MdContentCopy } from 'react-icons/md'
 
 export default function Offer(  ) {
+    const [click, setClick] = useState( false )
+    const handleCopy = () => {
+        navigator.clipboard.writeText('SPRING')
+        setClick(true)
+    }
+
     return (
         <div className="special-offers">
             <video autoPlay muted loop src={`${process.env.PUBLIC_URL}/spring_days.mp4`}></video>
             <div className="offer">
                 <h2>Spring Days</h2>
-                <p>-50% sur la 2ème paire avec le code promo :<br/>SPRING <MdContentCopy/></p>
+                <p>-50% sur la 2ème paire avec le code promo :<br/><span onClick={handleCopy}>SPRING <MdContentCopy/></span></p>
+                {click &&
+                    <p>Le code promo à bien été copier dans votre presse papier.</p>
+                }
             </div>
         </div>
     )
